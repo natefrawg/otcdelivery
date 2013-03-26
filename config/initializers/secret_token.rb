ab861cbdc986b462aa5007d6ed9cde8766ab8db0
Nathan Schuett <nschuett@gmail.com>
2013-03-26 00:23:49 -0700

    Update .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Otcdelivery::Application.config.secret_token = '4a8f4cb0590db8ac183270972df08e46f8a57866396cd4c39fdcbee3d89c8ca31c187902a9f244dd76dd572facc0cbe1b17a98a2a0ab202adbf7570d1695d1b5'
