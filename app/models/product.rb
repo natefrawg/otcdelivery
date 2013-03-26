dee0f9dbc70c16a40bf3c5575f1c7899f36a6076
Nathan Schuett <nschuett@gmail.com>
2013-03-26 13:27:47 -0700

    Update typo in pages controller

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,8 @@
 class Product < ActiveRecord::Base
   attr_accessible :product_name, :description, :url
 
+  validates :product_name, presence:true
   validates :description, presence:true
   validates :url, presence:true
-  validates :product_name, presence:true
+
 end
