b7cbce70377185c670e285621df306cb13962921
Nathan Schuett <nschuett@gmail.com>
2013-04-01 13:10:53 -0700

    Update product ordering, change button to order, update About page copy

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    @products = Product.order("products.product_name")
 
     respond_to do |format|
       format.html # index.html.erb
